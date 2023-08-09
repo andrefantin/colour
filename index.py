090e7c036995632365886290bcff5c512c527faf
andrefantin <andre@fant.in>
2023-08-09 20:02:48 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,22 +1,43 @@
-def calculate_color_contrast(contrast):
-  """Calculates the color contrast value based on the contrast against black.
+from decimal import Decimal
 
-  Args:
-    contrast: The contrast against black.
+def hex_to_rgb(hex_color):
+    hex_color = hex_color.lstrip('#')
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
 
-  Returns:
-    The color contrast value.
-  """
+def calculate_contrast_ratio(color1, color2):
+    def relative_luminance(color):
+        color = Decimal(color) / 255
+        return color / Decimal('12.92') if color <= Decimal('0.03928') else ((color + Decimal('0.055')) / Decimal('1.055')) ** Decimal('2.4')
 
-  value = ((contrast - 1) / 20) * 1000
-  return round(value, 1)
+    r1, g1, b1 = color1
+    r2, g2, b2 = color2
 
-def main():
-  """The main function."""
+    l1 = relative_luminance(r1) * Decimal('0.2126') + relative_luminance(g1) * Decimal('0.7152') + relative_luminance(b1) * Decimal('0.0722')
+    l2 = relative_luminance(r2) * Decimal('0.2126') + relative_luminance(g2) * Decimal('0.7152') + relative_luminance(b2) * Decimal('0.0722')
 
-  contrast = float(input("Insert the colour contrast against black: "))
-  color_contrast_value = calculate_color_contrast(contrast)
-  print("The color contrast value is:", color_contrast_value)
+    if l1 > l2:
+        brighter = l1
+        darker = l2
+    else:
+        brighter = l2
+        darker = l1
 
-if __name__ == "__main__":
-  main()
+    contrast_ratio = (brighter + Decimal('0.05')) / (darker + Decimal('0.05'))
+    return contrast_ratio
+
+def calculate_value(contrast_ratio):
+    value = ((float(contrast_ratio) - 1) / 20) * 1000
+    return round(value)
+
+def check_contrast(hex_color):
+    color1 = hex_to_rgb(hex_color)
+    color2 = hex_to_rgb("#000000")  # Black color
+    contrast_ratio = calculate_contrast_ratio(color1, color2)
+    value = calculate_value(contrast_ratio)
+    
+    return contrast_ratio, value
+
+hex_color = input("Enter the HEX color value: ")
+contrast_ratio, value = check_contrast(hex_color)
+print(f"Contrast ratio against black (#000000): {contrast_ratio:.2f}")
+print(f"Value calculated: {value}")
